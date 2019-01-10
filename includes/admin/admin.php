bfd9f5f17d94a4a4d7880a1cbec085feae6e5fe0
Justin Estrada <justin@taylordigital.com>
2019-01-09 23:46:27 -0800

    Fixed admin users followers/following columns

--- includes/admin/admin.php
+++ includes/admin/admin.php
@@ -10,10 +10,10 @@ add_filter( 'manage_users_columns', 'fdfp_update_user_table' );
 function fdfp_update_user_table_row( $val, $column_name, $user_id ) {
     switch ($column_name) {
         case 'followers' :
-            return fdfp_get_following_count( $user_id );
+            return fdfp_get_follower_count( $user_id );
             break;
         case 'following' :
-            return fdfp_get_follower_count( $user_id );
+            return fdfp_get_following_count( $user_id );
             break;
         default:
     }
